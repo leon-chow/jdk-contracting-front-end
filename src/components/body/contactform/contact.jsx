22e9541b76090e7d541fe7f543d7dfe1151e4c91
Leon <leon.chow1@ontariotechu.net>
2022-12-18 00:17:22 -0500

    Add FAQ text and change font sizes

--- src/components/body/contactform/contact.jsx
+++ src/components/body/contactform/contact.jsx
@@ -3,11 +3,23 @@ import "./contact.css";
 const ContactForm = () => {
   return (
     <div>
-      <h1 className="form-header">FAQ</h1>
+      <h1 className="form-header">Frequently Asked Questions </h1>
+      <h3> Why choose our services? </h3>
+      <p>
+        We are quick to respond and we have many happy customers! Feel free to
+        call us for a <b> FREE </b> site quote and we will be within your
+        location in a few business days. Feel free to reach out using the email
+        below!
+      </p>
       <h3> How do I get my quote? </h3>
       <p>
         Send me an email with the photos for rooms and the measurements that you
-        want to change, or send me an email below!
+        want to change using the link below!
+      </p>
+      <h3> How does payment work? </h3>
+      <p>
+        After consulting our quotes and choosing our rates, we will charge half
+        upfront and collect the rest when it's a job well done!
       </p>
       <h1 className="form-header">Contact Us!</h1>
       <a className="email-link" href="mailto:joshuakotsopoulos1@gmail.com">
