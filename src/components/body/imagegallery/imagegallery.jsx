ee05a122d800803bb847cc5752f0f6a9d794bedc
Leon <leon.chow1@ontariotechu.net>
2023-01-05 01:09:51 -0500

    Disable scroll on modal open, refactor galleries to use nested component instead of carousel

--- src/components/body/imagegallery/imagegallery.jsx
+++ src/components/body/imagegallery/imagegallery.jsx
@@ -1,9 +1,11 @@
 import "./imagegallery.css";
-import ImageCarousel from "../imagecarousel/imagecarousel";
 import { useState } from "react";
+
+import { CarouselComponents } from "../imagecarousel/imagecarousel";
 
 const ImageGallery = (props) => {
   const [displayImageGallery, setDisplayImageGallery] = useState(false);
+  const [carouselIndex, setCarouselIndex] = useState(0);
   return (
     <div className="gallery">
       <div className="overlay">
@@ -13,20 +15,51 @@ const ImageGallery = (props) => {
         className="gallery-image"
         alt="gallery Img"
         src={props.galleryImage}
-        onClick={() => setDisplayImageGallery((prevState) => !prevState)}
+        onClick={() =>
+          setDisplayImageGallery(
+            (prevState) => (prevState = true),
+            (document.body.style.overflow = "hidden")
+          )
+        }
       />
+      {/* TODO: Fix vertical overflow for galleries and increase carousel size of galleries */}
       {displayImageGallery && props.imageList ? (
         <div className="gallery-modal">
           <span
             className="close-button"
             onClick={() =>
-              setDisplayImageGallery((prevState) => (prevState = false))
+              setDisplayImageGallery(
+                (prevState) => (prevState = false),
+                (document.body.style.overflow = "auto")
+              )
             }
           >
             X
           </span>
           <div className="gallery-modal-content">
-            <ImageCarousel images={props.imageList} />
+            <div className="gallery-modal-container">
+              {CarouselComponents(carouselIndex, props.imageList)}
+              <span
+                onClick={() =>
+                  setCarouselIndex((prevState) =>
+                    prevState === 0
+                      ? props.imageList.length - 1
+                      : (prevState -= 1)
+                  )
+                }
+                className="arrow gallery-arrow-left"
+              ></span>
+              <span
+                className="arrow gallery-arrow-right"
+                onClick={() =>
+                  setCarouselIndex((prevState) =>
+                    prevState === props.imageList.length - 1
+                      ? 0
+                      : (prevState += 1)
+                  )
+                }
+              ></span>
+            </div>
           </div>
         </div>
       ) : null}
